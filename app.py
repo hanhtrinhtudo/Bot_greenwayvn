9428ef1e53801b1836428d278f46bd74ecfc0a44
hanhtrinhtudo <hanhtrinhtudo2@gmail.com>
2025-12-02 18:45:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1259,10 +1259,10 @@ def openai_chat():
         return jsonify({"reply": reply_text})
 
     except Exception as e:
-    import traceback
-    print("❌ ERROR /openai-chat:", e)
-    print(traceback.format_exc())
-        return jsonify(
+        import traceback
+        print("❌ ERROR /openai-chat:", e)
+        print(traceback.format_exc())
+            return jsonify(
             {
                 "reply": "Xin lỗi, hiện tại hệ thống đang gặp lỗi. Anh/chị vui lòng thử lại sau nhé."
             }
@@ -1397,5 +1397,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
 
